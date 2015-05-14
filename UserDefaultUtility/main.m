9575b1dc7266e00d498d6c3252665098d1bf6bd3
Wei Han <xingheng907@hotmail.com>
2015-05-14 13:49:31 +0800

    Initial Commit

---
+++ UserDefaultUtility/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UserDefaultUtility
+//
+//  Created by Wei Han on 5/14/15.
+//  Copyright (c) 2015 Wei Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
