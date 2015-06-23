fc12c2e04fe7387f1c29b17c941f5d3956be4c1d
Wei Han <xingheng907@hotmail.com>
2015-06-23 11:28:09 +0800

    Added source project into core files.

--- UserDefaultUtility/Classes/UserDefault.h
+++ UserDefaultUtility/Classes/UserDefault.h
@@ -3,7 +3,9 @@
 //  UserDefaultUtility
 //
 //  Created by Wei Han on 5/14/15.
-//  Copyright (c); 2015 Wei Han. All rights reserved.
+//  Copyright (c) 2015 Wei Han. All rights reserved.
+//
+//  Source project: https://github.com/xingheng/UserDefault
 //
 
 #import <Foundation/Foundation.h>
--- UserDefaultUtility/Classes/UserDefault.m
+++ UserDefaultUtility/Classes/UserDefault.m
@@ -5,6 +5,8 @@
 //  Created by Wei Han on 5/14/15.
 //  Copyright (c) 2015 Wei Han. All rights reserved.
 //
+//  Source project: https://github.com/xingheng/UserDefault
+//
 
 #import "UserDefault.h"
 
